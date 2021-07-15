a4a7413302778399e68255cef6bf51f242bca775
Stephen Zhang <x.stephen.zhang@gmail.com>
2021-07-15 15:44:32 -0400

    updated

--- functions/photometryplot.m
+++ functions/photometryplot.m
@@ -1,6 +1,8 @@
 function photometryplot(photometrypath, scoringpath, varargin)
 %photometryplot is a teaching function to plot scammel lab photometry data
 %   photometryplot(photometrypath, scoringpath, varargin)
+% Written by Stephen Zhang 7/2021
+
 
 % Take care of inputs or the lack of, so that you can just F5 the function
 % and run it (better to call this function with a script)
@@ -22,7 +24,11 @@ p = inputParser;
 addOptional(p, 'defaultpath', 'D:\Data'); % Give default path for where the data are to be found
 addOptional(p, 'photometryext', '*.csv'); % Photometry extension
 addOptional(p, 'scoringext', '*.xlsx'); % Scoring extension
-addOptional(p, 'transitions', {'W', 'NR', 1; 'W', 'NR', 10}); % A cell containing the transition info.
+addOptional(p, 'window', [60, 60]); % Window size (pre, post)
+addOptional(p, 'transitions', {'W', 'NR', [0, inf, 0, 60]}); % A cell containing the transition info.
+                                                             % e.g., {'W', 'NR', [0, inf, 0, 60]}
+                                                             % means no limit for pre-transition bouts and
+                                                             % 0-60 seconds for post-transition bout lengths
 
 % Photometry variables
 addOptional(p, 'tcolumn', 1); % time column
@@ -30,6 +36,12 @@ addOptional(p, 'ch1column', 2); % channel 1 column
 addOptional(p, 'ch2column', 3); % channel 2 column
 addOptional(p, 'usezscore', true); % zscore photometry
 addOptional(p, 'zscoreperrow', false); % zscore per row (per transition)
+
+% Scoring file columns
+addOptional(p, 'onsetcolumn', 5); % Onsets
+addOptional(p, 'durationcolumn', 4); % Durations
+addOptional(p, 'lagcolumn', 6); % lag column (how many seconds that scoring lags behind photometry)
+
 
 % Plotting variables
 addOptional(p, 'subplotrows', 6); % Proportion of heatmap vs average
@@ -92,10 +104,15 @@ t = toc;
 fprintf('Done. Elapsed time = %i seconds.\n', round(t));
 
 % Parse scoring
-onsets = scoringdata(:,5);
-durations = scoringdata(:,4);
+onsets = scoringdata(:,p.onsetcolumn);
+durations = scoringdata(:,p.durationcolumn);
+scoringlag = scoringdata(1,p.lagcolumn);
+onsets = onsets + scoringlag;
 labels = scoringtext(2:end,3);
+
+% Make transitions
 labelpairs = [labels(1:end-1), labels(2:end)];
+durationpairs = [durations(1:end-1), durations(2:end)];
 
 % Parse photometry
 ch1 = photometrydata(:,p.ch1column);
@@ -131,167 +148,180 @@ nplots = size(p.transitions, 1);
 
 for iplot = 1 : nplots
     % Find the right type of sessions
+    % Judging by type
     ipre = strcmpi(labelpairs(:,1), p.transitions{iplot, 1});
     ipost = strcmpi(labelpairs(:,2), p.transitions{iplot, 2});
-    inds = find(ipre & ipost) + 1;
     
-    % Onsets of the current transitions(in seconsd)
-    consets = onsets(inds);
-    nonsets = length(consets);
+    % Judging by durations
+    dpre1 = durationpairs(:,1) >= p.transitions{iplot, 3}(1);
+    dpre2 = durationpairs(:,1) < p.transitions{iplot, 3}(2);
+    dpost1 = durationpairs(:,2) >= p.transitions{iplot, 3}(3);
+    dpost2 = durationpairs(:,2) < p.transitions{iplot, 3}(4);
     
-    % Durations of pre- and post-transition bouts
-    dur_pre = durations(inds-1);
-    dur_post = durations(inds);
+    inds = find(ipre & ipost & dpre1 & dpre2 & dpost1 & dpost2);
     
-    % Apply the window of interest to the durations
-    dur_pre = min(dur_pre, p.transitions{iplot, 3});
-    dur_post = min(dur_post, p.transitions{iplot, 3});
-    
-    % Turn seconds into indices
-    inds_pre = round(dur_pre * fs);
-    inds_post = round(dur_post * fs);
-    consets_ind = zeros(nonsets,1);
-    for i = 1 : nonsets
-        [~, consets_ind(i)] = min(abs(time - consets(i)));
-    end
-    
-    % Initialize matrix to load the data
-    l_pre = round(p.transitions{iplot, 3} * fs);
-    l_post = round(p.transitions{iplot, 3} * fs);
-    l_all = l_pre + l_post + 1;
-    ch1_mat = nan(nonsets, l_all);
-    ch2_mat = nan(nonsets, l_all);
-    
-    % Loop through each transition and gather data
-    for i = 1 : nonsets
-        % Indices for current transition
-        currind_start = consets_ind(i) - l_pre;
-        currind_stop = consets_ind(i) + l_post;
-        
-        % Since not all transitions fit the lengths (some might be
-        % shorter), we need to add empty numbers to the front and the end
-        % if necessary
-        if currind_start < 1
-            prebuffer = nan(1, -currind_start + 1);
-            r1 = horzcat(prebuffer, ch1(1 : currind_stop)');
-            r2 = horzcat(prebuffer, ch2(1 : currind_stop)');
-        elseif currind_stop > l
-            postbuffer = nan(1, currind_stop - l);
-            r1 = horzcat(ch1(currind_start : l)', postbuffer);
-            r2 = horzcat(ch2(currind_start : l)', postbuffer);
-        else
-            r1 = ch1(currind_start : currind_stop)';
-            r2 = ch2(currind_start : currind_stop)';
-        end
-        
-        % Load each row into the datamatrix
-        if p.zscoreperrow
-            ch1_mat(i,:) = tcpZscore(r1);
-            ch2_mat(i,:) = tcpZscore(r2);
-        else
-            ch1_mat(i,:) = r1;
-            ch2_mat(i,:) = r2;
-        end
-        
-    end
-    
-    %% Making figures
-    % Initialize figure
-    figure
-        
-    % Loop through
-    for i = 1 : 2
-        % Subplot for imagesc
-        subplot(p.subplotrows, 2, (2 + i) : 2 : (2 * p.subplotrows));
-
-        % Imagesc
-        if i == 1
-            imagesc(ch1_mat);
-        else
-            imagesc(ch2_mat);
-        end
-        
-        % Color range
-        if ~isempty(p.heatmaprange)
-            colormap(b2r_arbitrary_input(p.heatmaprange(1), p.heatmaprange(2), [1 0 0], [0 0 1], [1 1 1]));
-        else
-            crange = caxis();
-            colormap(b2r_arbitrary_input(crange(1), crange(2), [1 0 0], [0 0 1], [1 1 1]));
-        end
-        
-        % Get xaxis range and ticks
-        xrange = get(gca,'xlim');
-        set(gca, 'XTick', [1, l_pre+1, l_pre+l_post]);
-        
-        % Set the xaxis right
-        xlabel('Time (s)')
-        set(gca,'XTickLabel', num2cell([-p.transitions{iplot, 3}, 0, p.transitions{iplot, 3}]));
-        
-        % Draw event lines
-        hold on
-        for j = 1 : nonsets
-            % Draw pre-transition event line
-            plot([l_pre-inds_pre(j), l_pre+1], [j j], 'g-', 'LineWidth', p.linethickness);
+    if ~isempty(inds) % If no such sessions are found - skip
             
-            % Draw post-transition event line
-            plot([l_pre+1, l_pre+inds_post(j)], [j j], 'm-', 'LineWidth', p.linethickness);
-        end
-        
-        % Draw a zero line
-        plot([l_pre+1, l_pre+1], [0.5 nonsets+0.5], 'k-');
-        hold off
+        % Onsets of the current transitions(in seconsd)
+        consets = onsets(inds+1);
+        nonsets = length(consets);
 
-        % Subplot 2
-        % Subplot for average data
-        subplot(p.subplotrows, 2, i)
+        % Durations of pre- and post-transition bouts
+        dur_pre = durationpairs(inds,1);
+        dur_post = durationpairs(inds,2);
 
-        % Plot average data
-        if i == 1
-            datavec = nanmean(ch1_mat,1);
-        else
-            datavec = nanmean(ch2_mat,1);
-        end
-        plot(datavec);
-        
-        % Add an y = 0 line and an x = 0 line
-        hold on
-        plot(xrange, [0 0], 'Color', [0 0 0]);
-        plot([l_pre+1, l_pre+1], [min(datavec), max(datavec)], 'k-');
-        hold off
+        % Limit the durations to within window
+        dur_pre = min(dur_pre, p.window(1));
+        dur_post = min(dur_post, p.window(2));
 
-        % Align ranges
-        xlim(xrange);
-        set(gca, 'XTick', [1, l_pre+1, l_pre+l_post]);
-        
-        % Use channel 1 to set yrange
-        if i == 1
-            yrange = [min(datavec), max(datavec)];
+        % Turn seconds into indices
+        inds_pre = round(dur_pre * fs);
+        inds_post = round(dur_post * fs);
+        consets_ind = zeros(nonsets,1);
+        for i = 1 : nonsets
+            [~, consets_ind(i)] = min(abs(time - consets(i)));
         end
-        ylim(yrange);
 
-        set(gca,'XTickLabel',[]);
-        
-        % title
-        title(sprintf('%s - %s', p.transitions{iplot, 1}, p.transitions{iplot, 2}))
-    end
-    
-    % Save plots
-    if p.savefig
-        [fp, ~, ~] = fileparts(scoringpath);
-        figfp = fullfile(fp, 'Plots');
-        if ~exist(figfp, 'dir')
-            mkdir(figfp);
+        % Initialize matrix to load the data
+        l_pre = round(p.window(1) * fs);
+        l_post = round(p.window(2) * fs);
+        l_all = l_pre + l_post + 1;
+        ch1_mat = nan(nonsets, l_all);
+        ch2_mat = nan(nonsets, l_all);
+
+        % Loop through each transition and gather data
+        for i = 1 : nonsets
+            % Indices for current transition
+            currind_start = consets_ind(i) - l_pre;
+            currind_stop = consets_ind(i) + l_post;
+
+            % Since not all transitions fit the lengths (some might be
+            % shorter), we need to add empty numbers to the front and the end
+            % if necessary
+            if currind_start < 1
+                prebuffer = nan(1, -currind_start + 1);
+                r1 = horzcat(prebuffer, ch1(1 : currind_stop)');
+                r2 = horzcat(prebuffer, ch2(1 : currind_stop)');
+            elseif currind_stop > l
+                postbuffer = nan(1, currind_stop - l);
+                r1 = horzcat(ch1(currind_start : l)', postbuffer);
+                r2 = horzcat(ch2(currind_start : l)', postbuffer);
+            else
+                r1 = ch1(currind_start : currind_stop)';
+                r2 = ch2(currind_start : currind_stop)';
+            end
+
+            % Load each row into the datamatrix
+            if p.zscoreperrow
+                ch1_mat(i,:) = tcpZscore(r1);
+                ch2_mat(i,:) = tcpZscore(r2);
+            else
+                ch1_mat(i,:) = r1;
+                ch2_mat(i,:) = r2;
+            end
+
         end
-        
-        % output figure
-        figfn = sprintf('%s-%s_%is.png', p.transitions{iplot, 1}, p.transitions{iplot, 2},...
-            p.transitions{iplot, 3});
-        
-        % save
-        saveas(gcf, fullfile(figfp, figfn));
-        
-        if p.closefigaftersave
-            close(gcf)
+
+        %% Making figures
+        % Initialize figure
+        figure
+
+        % Loop through
+        for i = 1 : 2
+            % Subplot for imagesc
+            subplot(p.subplotrows, 2, (2 + i) : 2 : (2 * p.subplotrows));
+
+            % Imagesc
+            if i == 1
+                imagesc(ch1_mat);
+            else
+                imagesc(ch2_mat);
+            end
+
+            % Color range
+            if ~isempty(p.heatmaprange)
+                colormap(b2r_arbitrary_input(p.heatmaprange(1), p.heatmaprange(2), [1 0 0], [0 0 1], [1 1 1]));
+            else
+                crange = caxis();
+                colormap(b2r_arbitrary_input(crange(1), crange(2), [1 0 0], [0 0 1], [1 1 1]));
+            end
+
+            % Get xaxis range and ticks
+            xrange = get(gca,'xlim');
+            set(gca, 'XTick', [1, l_pre+1, l_pre+l_post]);
+
+            % Set the xaxis right
+            xlabel('Time (s)')
+            set(gca,'XTickLabel', num2cell([-p.window(1), 0, p.window(1)]));
+
+            % Draw event lines
+            hold on
+            for j = 1 : nonsets
+                % Draw pre-transition event line
+                plot([l_pre-inds_pre(j), l_pre+1], [j j], 'g-', 'LineWidth', p.linethickness);
+
+                % Draw post-transition event line
+                plot([l_pre+1, l_pre+inds_post(j)], [j j], 'm-', 'LineWidth', p.linethickness);
+            end
+
+            % Draw a zero line
+            plot([l_pre+1, l_pre+1], [0.5 nonsets+0.5], 'k-');
+            hold off
+
+            % Subplot 2
+            % Subplot for average data
+            subplot(p.subplotrows, 2, i)
+
+            % Plot average data
+            if i == 1
+                datavec = nanmean(ch1_mat,1);
+            else
+                datavec = nanmean(ch2_mat,1);
+            end
+            plot(datavec);
+
+            % Add an y = 0 line and an x = 0 line
+            hold on
+            plot(xrange, [0 0], 'Color', [0 0 0]);
+            plot([l_pre+1, l_pre+1], [min(datavec), max(datavec)], 'k-');
+            hold off
+
+            % Align ranges
+            xlim(xrange);
+            set(gca, 'XTick', [1, l_pre+1, l_pre+l_post]);
+
+            % Use channel 1 to set yrange
+            if i == 1
+                yrange = [min(datavec), max(datavec)];
+            end
+            ylim(yrange);
+
+            set(gca,'XTickLabel',[]);
+
+            % title
+            title(sprintf('Ch%i: %s (%i-%i) - %s (%i-%i)', i, p.transitions{iplot, 1},...
+                p.transitions{iplot, 3}(1), p.transitions{iplot, 3}(2),...
+                p.transitions{iplot, 2}, p.transitions{iplot, 3}(3), p.transitions{iplot, 3}(4)));
+        end
+
+        % Save plots
+        if p.savefig
+            [fp, ~, ~] = fileparts(scoringpath);
+            figfp = fullfile(fp, 'Plots');
+            if ~exist(figfp, 'dir')
+                mkdir(figfp);
+            end
+
+            % output figure
+            figfn = sprintf('%s-%s_%i.png', p.transitions{iplot, 1}, p.transitions{iplot, 2},...
+                iplot);
+
+            % save
+            saveas(gcf, fullfile(figfp, figfn));
+
+            if p.closefigaftersave
+                close(gcf)
+            end
         end
     end
 end
--- script_for_chris.m
+++ script_for_chris.m
@@ -1,19 +1,19 @@
 % Inputs
-photometrypath = 'I:\For Stephen From Chris\CC_OX_DTR_GRAB_5HT_MCHERRY_8_AM_Photometry_data.csv';
-scoringpath = 'I:\For Stephen From Chris\CC_OX_DTR_GRAB_5HT_MCHERRY_8_Baseline_AM_SS_OPT.xlsx';
+photometrypath = 'I:\For Stephen from Chris 2\OX_DTR_GRAB_5HT_MCHERRY_8_Baseline_06152021AM2hrs_PhotometryMATLAB.csv';
+scoringpath = 'I:\For Stephen from Chris 2\CC_OX_DTR_GRAB_5HT_MCHERRY_8_Baseline_AM_SS_Transitions.xlsx';
 
 % Debug
 % [scoringdata, scoringtext, ~] = xlsread(scoringpath);
 % [photometrydata, ~, ~] = xlsread(photometrypath);
-
+%%
 % Define transitions
-transitions = {'W', 'NR', 60; 'W', 'NR', 600; 'W', 'NR', 1800;...
-               'NR', 'W', 60; 'NR', 'W', 600; 'NR', 'R', 1800;...
-               'R', 'W', 10; 'NR', 'R', 60; 'NR', 'R', 600};
+transitions = {'W', 'NR', [0,inf,0,60]; 'W', 'NR', [0,inf,60,600]; 'W', 'NR', [0,inf,600,inf];...
+               'NR', 'W', [0,inf,0,60]; 'NR', 'W', [0,inf,60,600]; 'NR', 'R', [0,inf,600,inf];...
+               'R', 'W', [0,inf,0,10]; 'NR', 'R', [0,inf,0,60]; 'NR', 'R', [0,inf,60,inf]};
            
 % Paramters (read photometryplot.m for what they mean)
 varargin = {'transitions', transitions, 'savefig', true, 'heatmaprange', [-4 4],...
-    'downsample', true, 'fs', 2, 'zscoreperrow', true, 'closefigaftersave', true};
+    'downsample', true, 'fs', 5, 'zscoreperrow', true, 'closefigaftersave', true};
 
 % Run the function
 photometryplot(photometrypath, scoringpath, varargin)
